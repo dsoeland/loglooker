8fa66819af6799beeddf983c5edc486a6be0c60c
Hinn <dsgenericemail@gmail.com>
2025-06-05 14:35:43 -0700

    feat: Add init Spring and Angular projects

---
+++ loglooker-backend/src/test/java/com/loglooker/backend/LoglookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loglooker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoglookerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
